9542712e8d36bdd7c34ba1f55c44f716d597a964
Mabuljo <151923541+Mabuljo@users.noreply.github.com>
2024-06-13 09:25:16 +0200

    MAJ de l'initialisation du projet

---
+++ src/components/App.js
@@ -0,0 +1,20 @@
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "../pages/Home";
+import About from "../pages/About";
+import Error from "../pages/Error";
+
+const App = () => {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        {/* path="*" fonctionne si jamais l'url ne correspond à rien de déclaré au dessus */}
+        <Route path="*" element={<Error />} />
+      </Routes>
+    </BrowserRouter>
+  );
+};
+
+export default App;
