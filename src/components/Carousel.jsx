c90e6fbe25c43332aadeaab02f112f70bfbbb849
Mabuljo <151923541+Mabuljo@users.noreply.github.com>
2024-07-17 15:00:37 +0200

    correction d'un erreur dans Carousel.jsx

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -8,12 +8,12 @@ const Carousel = ({pictures}) => {
 
     return (
         <div className="carousel">
-            {picturesLength > 1 && (
-                <>
-                {pictures.map((slideActive, index) => (
+            {pictures.map((slideActive, index) => (
                 index === picture &&
                 <img key={index} src={slideActive} alt="appartement à louer" className="carousel__picture"/>
                 ))}
+            {picturesLength > 1 && (
+                <>
                 <img src={arrowLeft} alt="Flèche de gauche" className="arrow arrow-left" onClick = {() => setPicture(picture === 0? picturesLength -1 : picture -1)} /> 
                 <img src={arrowRight} alt="Flèche de droite" className="arrow arrow-right" onClick = {() => setPicture(picture === picturesLength -1? 0 : picture +1)} />
                 <div className="carousel__compteur">
